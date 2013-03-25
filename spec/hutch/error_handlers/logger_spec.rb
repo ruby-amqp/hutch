79c74b3051cc99d0f7ada8fbd730b5ac9ea49612
Karl Sutt <karl@sutt.ee>
2013-03-25 16:09:17 +0000

    Change spec description

--- spec/hutch/error_handlers/logger_spec.rb
+++ spec/hutch/error_handlers/logger_spec.rb
@@ -6,7 +6,7 @@ describe Hutch::ErrorHandlers::Logger do
                        backtrace: ["line 1", "line 2"]) }
     subject { described_class.new.handle("1", stub, error) }
 
-    it "logs three separate lines to stdout" do
+    it "logs three separate lines" do
       Hutch::Logging.logger.should_receive(:warn).exactly(3).times
       subject
     end
