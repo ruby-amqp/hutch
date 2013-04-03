3d439b2e4c455159a0bcc39e0f95d36e4c6e3342
Harry Marr <harry.marr@gmail.com>
2013-04-03 14:53:14 +0100

    Modulize Message

--- lib/hutch/message.rb
+++ lib/hutch/message.rb
@@ -1,24 +1,26 @@
 require 'multi_json'
 require 'forwardable'
 
-class Message
-  extend Forwardable
+module Hutch
+  class Message
+    extend Forwardable
 
-  def initialize(delivery_info, properties, payload)
-    @delivery_info = delivery_info
-    @properties    = properties
-    @payload       = payload
-    @body          = MultiJson.load(payload, symbolize_keys: true)
-  end
+    def initialize(delivery_info, properties, payload)
+      @delivery_info = delivery_info
+      @properties    = properties
+      @payload       = payload
+      @body          = MultiJson.load(payload, symbolize_keys: true)
+    end
 
-  def_delegator :@body, :[]
-  def_delegators :@properties, :message_id, :timestamp
-  def_delegators :@delivery_info, :routing_key, :exchange
+    def_delegator :@body, :[]
+    def_delegators :@properties, :message_id, :timestamp
+    def_delegators :@delivery_info, :routing_key, :exchange
 
-  attr_reader :body
+    attr_reader :body
 
-  def to_s
-    "#<Message #{body.map { |k,v| "#{k}: #{v.inspect}" }.join(', ')}>"
+    def to_s
+      "#<Message #{body.map { |k,v| "#{k}: #{v.inspect}" }.join(', ')}>"
+    end
   end
 end
 
