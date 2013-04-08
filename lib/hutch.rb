c3e4cbccd60150eeb163a60eda522bcbbe21bef7
Karl Sutt <karl@sutt.ee>
2013-04-08 10:56:36 +0000

    Require Sentry backend iff Raven is defined

--- lib/hutch.rb
+++ lib/hutch.rb
@@ -3,7 +3,7 @@ module Hutch
   require 'hutch/worker'
   require 'hutch/logging'
   require 'hutch/error_handlers/logger'
-  require 'hutch/error_handlers/sentry'
+  require 'hutch/error_handlers/sentry' if defined?(Raven)
 
   def self.register_consumer(consumer)
     self.consumers << consumer
