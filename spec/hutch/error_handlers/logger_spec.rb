568cd7c53fabbda82a8e598bcac1ed87db5a03d6
Karl Sutt <karl@sutt.ee>
2013-04-08 12:02:05 +0000

    Refactor error backend specs

--- spec/hutch/error_handlers/logger_spec.rb
+++ spec/hutch/error_handlers/logger_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 describe Hutch::ErrorHandlers::Logger do
+  let(:error_handler) { Hutch::ErrorHandlers::Logger.new }
+
   describe '#handle' do
     let(:error) { stub(message: "Stuff went wrong", class: "RuntimeError",
                        backtrace: ["line 1", "line 2"]) }
-    subject { described_class.new.handle("1", stub, error) }
 
     it "logs three separate lines" do
-      Hutch::Logging.logger.should_receive(:warn).exactly(3).times
-      subject
+      Hutch::Logging.logger.should_receive(:error).exactly(3).times
+      error_handler.handle("1", stub, error)
     end
   end
 end
--- spec/hutch/error_handlers/sentry_spec.rb
+++ spec/hutch/error_handlers/sentry_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 describe Hutch::ErrorHandlers::Sentry do
+  let(:error_handler) { Hutch::ErrorHandlers::Sentry.new }
+
   describe '#handle' do
     let(:error) { stub(message: "Stuff went wrong", class: "RuntimeError",
                        backtrace: ["line 1", "line 2"]) }
     let(:event) { stub }
-    subject { described_class.new.handle("1", stub, error) }
 
     it "logs the error to Sentry" do
       Raven::Event.should_receive(:capture_exception).with(error).
                    and_return(event)
       Raven.should_receive(:send).with(event)
-      subject
+      error_handler.handle("1", stub, error)
     end
   end
 end
