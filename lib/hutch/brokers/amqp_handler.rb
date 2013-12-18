2c98e8c0813573f0164653177dfa1ee06b8d1b18
Benjamin Roth <apnea.diving.deep@gmail.com>
2013-12-18 15:34:56 +0100

    refactored broker

---
+++ lib/hutch/brokers/amqp_handler.rb
@@ -0,0 +1,54 @@
+require 'delegate'
+
+module Hutch
+  module Brokers
+    class AmqpHandler < SimpleDelegator
+
+      def connection_log_info
+        "connecting to rabbitmq (#{protocol}#{uri})"
+      end
+
+      def exchange_log_info
+        "using topic exchange '#{ mq_exchange }'"
+      end
+
+      def connection_error
+        "couldn't connect to rabbitmq at #{ short_uri }"
+      end
+
+      def bunny_params
+        {
+          host:      mq_host,
+          port:      mq_port,
+          vhost:     mq_vhost,
+          tls:       mq_tls,
+          tls_key:   mq_tls_key,
+          tls_cert:  mq_tls_cert,
+          username:  mq_username,
+          password:  mq_password,
+          heartbeat: 30,
+          automatically_recover: true,
+          network_recovery_interval: 1
+        }
+      end
+
+      private
+
+      def protocol
+         mq_tls ? "amqps://" : "amqp://"
+      end
+
+      def uri
+        "#{ mq_username }:#{ mq_password }@#{ mq_host }:#{ mq_port }/#{ vhost }"
+      end
+
+      def vhost
+        mq_vhost.sub(/^\//, '')
+      end
+
+      def short_uri
+        "#{ protocol }#{ mq_host }:#{ mq_port }"
+      end
+    end
+  end
+end
--- lib/hutch/config.rb
+++ lib/hutch/config.rb
@@ -7,68 +7,74 @@ module Hutch
   module Config
     require 'yaml'
 
-    def self.initialize
-      @config = {
-        mq_host: 'localhost',
-        mq_port: 5672,
-        mq_exchange: 'hutch',  # TODO: should this be required?
-        mq_vhost: '/',
-        mq_tls: false,
-        mq_tls_cert: nil,
-        mq_tls_key: nil,
-        mq_username: 'guest',
-        mq_password: 'guest',
-        mq_api_host: 'localhost',
-        mq_api_port: 15672,
-        mq_api_ssl: false,
-        log_level: Logger::INFO,
-        require_paths: [],
-        autoload_rails: true,
-        error_handlers: [Hutch::ErrorHandlers::Logger.new]
-      }
-    end
-
-    def self.get(attr)
-      check_attr(attr)
-      user_config[attr]
-    end
-
-    def self.set(attr, value)
-      check_attr(attr)
-      user_config[attr] = value
-    end
-
     class << self
+
+      def initialize
+        @config = {
+          mq_host: 'localhost',
+          mq_port: 5672,
+          mq_exchange: 'hutch',  # TODO: should this be required?
+          mq_vhost: '/',
+          mq_tls: false,
+          mq_tls_cert: nil,
+          mq_tls_key: nil,
+          mq_username: 'guest',
+          mq_password: 'guest',
+          mq_api_host: 'localhost',
+          mq_api_port: 15672,
+          mq_api_ssl: false,
+          log_level: Logger::INFO,
+          require_paths: [],
+          autoload_rails: true,
+          error_handlers: [Hutch::ErrorHandlers::Logger.new]
+        }
+      end
+
+      def get(attr)
+        check_attr(attr)
+        user_config[attr]
+      end
       alias_method :[],  :get
+
+      def set(attr, value)
+        check_attr(attr)
+        user_config[attr] = value
+      end
+
       alias_method :[]=, :set
-    end
 
-    def self.check_attr(attr)
-      unless user_config.key?(attr)
-        raise UnknownAttributeError, "#{attr} is not a valid config attribute"
+      def check_attr(attr)
+        unless user_config.key?(attr)
+          raise UnknownAttributeError, "#{attr} is not a valid config attribute"
+        end
       end
-    end
 
-    def self.user_config
-      initialize unless @config
-      @config
-    end
-
-    def self.load_from_file(file)
-      YAML.load(file).each do |attr, value|
-        Hutch::Config.send("#{attr}=", value)
+      def user_config
+        initialize unless @config
+        @config
       end
-    end
 
-    def self.method_missing(method, *args, &block)
-      attr = method.to_s.sub(/=$/, '').to_sym
-      return super unless user_config.key?(attr)
-
-      if method =~ /=$/
-        set(attr, args.first)
-      else
-        get(attr)
+      def load_from_file(file)
+        YAML.load(file).each do |attr, value|
+          Hutch::Config.send("#{attr}=", value)
+        end
       end
+
+      def method_missing(method, *args, &block)
+        attr = method.to_s.sub(/=$/, '').to_sym
+        return super unless user_config.key?(attr)
+
+        if method =~ /=$/
+          set(attr, args.first)
+        else
+          get(attr)
+        end
+      end
+
+      def each_pair(*args)
+        @config.each_pair(*args)
+      end
+
     end
 
     private
