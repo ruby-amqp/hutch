125bb6e183b4a48dd1a302d32503ed3ff6573e29
Daniel Farrell <danielfarrell76@gmail.com>
2014-12-23 10:23:10 -0500

    Add a honeybadger error handler

--- lib/hutch/error_handlers.rb
+++ lib/hutch/error_handlers.rb
@@ -1,6 +1,7 @@
 module Hutch
   module ErrorHandlers
-    autoload :Logger, 'hutch/error_handlers/logger'
-    autoload :Sentry, 'hutch/error_handlers/sentry'
+    autoload :Logger,      'hutch/error_handlers/logger'
+    autoload :Sentry,      'hutch/error_handlers/sentry'
+    autoload :Honeybadger, 'hutch/error_handlers/honeybadger'
   end
 end
