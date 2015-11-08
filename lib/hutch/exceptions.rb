e32dda40466cb397f0dc91385b6b3d33cbed7ca8
Michael Klishin <michael@clojurewerkz.org>
2015-11-08 12:19:35 +0300

    Make Hutch::Exception inherit from Bunny::Exception
    Fixes #137 (at least for some cases).
    
    Apps that explicitly handle Hutch::Exception now can do
    this and also cover Bunny exceptions.

--- lib/hutch/exceptions.rb
+++ lib/hutch/exceptions.rb
@@ -1,5 +1,8 @@
+require "bunny/exceptions"
+
 module Hutch
-  class Exception < StandardError; end
+  # Bunny::Exception inherits from StandardError
+  class Exception < Bunny::Exception; end
   class ConnectionError < Exception; end
   class AuthenticationError < Exception; end
   class WorkerSetupError < Exception; end
