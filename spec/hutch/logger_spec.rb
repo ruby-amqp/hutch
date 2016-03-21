7e0cdc3f24cf261cfde56665dfdf9494a507cb0a
Kelly Stannard <kelly.stannard@caspersleep.com>
2016-03-21 17:02:37 -0400

    use true null logger for specs

--- spec/hutch/logger_spec.rb
+++ spec/hutch/logger_spec.rb
@@ -3,24 +3,30 @@ require 'spec_helper'
 describe Hutch::Logging do
   let(:dummy_object) do
     class DummyObject
-      include Hutch::Logging
+      include described_class
     end
   end
 
   describe '#logger' do
+    around do |example|
+      old_logger = described_class.logger
+      described_class.setup_logger
+      example.run
+      described_class.logger = old_logger
+    end
+
     context 'with the default logger' do
-      subject { Hutch::Logging.logger }
+      subject { described_class.logger }
 
       it { is_expected.to be_instance_of(Logger) }
     end
 
     context 'with a custom logger' do
-      let(:dummy_logger) { double("Dummy logger", warn: true, info: true) }
-      after { Hutch::Logging.setup_logger }
+      let(:dummy_logger) { double("Dummy logger") }
 
       it "users the custom logger" do
-        Hutch::Logging.logger = dummy_logger
-        expect(Hutch::Logging.logger).to eq(dummy_logger)
+        described_class.logger = dummy_logger
+        expect(described_class.logger).to eq(dummy_logger)
       end
     end
   end
