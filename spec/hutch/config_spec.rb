692bb978c0daa14ea4dbe1e36f86e061c3da85d7
Olle Jonsson <olle.jonsson@ecraft.com>
2016-04-12 10:33:53 +0200

    Spec: Turn a describe into a context

--- spec/hutch/config_spec.rb
+++ spec/hutch/config_spec.rb
@@ -96,26 +96,21 @@ describe Hutch::Config do
         expect(Hutch::Config.mq_username).to eq username
       end
     end
-  end
 
-  describe '.load_from_file' do
-    let(:host) { 'localhost' }
-    let(:username) { 'calvin' }
-    let(:file) do
-      Tempfile.new('configs.yaml').tap do |t|
-        t.write(config_contents)
-        t.rewind
+    context 'when using ERB' do
+      let(:host) { 'localhost' }
+      let(:file) do
+        Tempfile.new('configs.yaml').tap do |t|
+          t.write(config_contents)
+          t.rewind
+        end
       end
-    end
-
-    context 'when using ERb' do
       let(:config_contents) do
         <<-YAML
 mq_host: 'localhost'
 mq_username: '<%= "calvin" %>'
-YAML
+        YAML
       end
-
       it 'loads in the config data' do
         Hutch::Config.load_from_file(file)
         expect(Hutch::Config.mq_host).to eq host
