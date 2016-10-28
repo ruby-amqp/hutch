5ac8222939bd26838cb77e1649537473788cbe17
Olle Jonsson <olle.jonsson@gmail.com>
2016-10-28 13:07:06 +0200

    Add an Opbeat Tracer

--- lib/hutch/tracers.rb
+++ lib/hutch/tracers.rb
@@ -2,5 +2,6 @@ module Hutch
   module Tracers
     autoload :NullTracer, 'hutch/tracers/null_tracer'
     autoload :NewRelic,   'hutch/tracers/newrelic'
+    autoload :Opbeat,     'hutch/tracers/opbeat'
   end
 end
