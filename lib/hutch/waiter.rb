e41aaa77fcf2470a9ad71caf5a29cd1e82e0f4f4
Olle Jonsson <olle.jonsson@gmail.com>
2016-11-18 11:45:13 +0100

    Waiter: add USER_SIGNALS USR1, USR2

--- lib/hutch/waiter.rb
+++ lib/hutch/waiter.rb
@@ -3,8 +3,12 @@ require 'hutch/logging'
 module Hutch
   class Waiter
     include Logging
-
+    
+    class SkipHere < Exception
+    end
+    
     SHUTDOWN_SIGNALS = %w(QUIT TERM INT).keep_if { |s| Signal.list.keys.include? s }.freeze
+    USER_SIGNALS = %w(USR1 USR2).keep_if { |s| Signal.list.keys.include?(s) }.freeze
 
     def self.wait_until_signaled
       new.wait_until_signaled
@@ -14,10 +18,20 @@ module Hutch
       self.sig_read, self.sig_write = IO.pipe
 
       register_signal_handlers
-      wait_for_signal
+      
+      begin
+        wait_for_signal
 
-      sig = sig_read.gets.strip.downcase
-      logger.info "caught sig#{sig}, stopping hutch..."
+        sig = sig_read.gets.strip.downcase
+        if USER_SIGNALS.include?(sig.upcase)
+          logger.info "FUN TIME! #{sig}"
+          raise SkipHere
+        else
+          logger.info "caught sig#{sig}, stopping hutch..."
+        end
+      rescue SkipHere
+        retry
+      end
     end
 
     private
@@ -29,7 +43,7 @@ module Hutch
     end
 
     def register_signal_handlers
-      SHUTDOWN_SIGNALS.each do |sig|
+      (SHUTDOWN_SIGNALS + USER_SIGNALS).each do |sig|
         # This needs to be reentrant, so we queue up signals to be handled
         # in the run loop, rather than acting on signals here
         trap(sig) do
